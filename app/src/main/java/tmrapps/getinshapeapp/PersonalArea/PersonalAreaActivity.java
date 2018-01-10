068d46889d44c209deccfa21592a7bd9b84fad0e
Nesh4 <riell@github.com>
2018-01-10 18:16:17 +0200

    HomePage

--- app/src/main/java/tmrapps/getinshapeapp/PersonalArea/PersonalAreaActivity.java
+++ app/src/main/java/tmrapps/getinshapeapp/PersonalArea/PersonalAreaActivity.java
@@ -1,5 +1,6 @@
 package tmrapps.getinshapeapp.PersonalArea;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +12,33 @@ public class PersonalAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_area);
+        setupTabs();
+    }
+
+    private void setupTabs() {
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(true);
+
+        ActionBar.Tab tab1 = actionBar
+                .newTab()
+                .setText("First")
+                //.setIcon(R.drawable.ic_home)
+                .setTabListener(
+                        new FragmentTabListener<HomePage>(R.id.tabLayout, this, "first",
+                                HomePage.class));
+
+        actionBar.addTab(tab1);
+        actionBar.selectTab(tab1);
+
+        ActionBar.Tab tab2 = actionBar
+                .newTab()
+                .setText("Second")
+                //.setIcon(R.drawable.ic_mentions)
+                .setTabListener(
+                        new FragmentTabListener<HomePage>(R.id.tabLayout, this, "second",
+                                HomePage.class));
+
+        actionBar.addTab(tab2);
     }
 }
