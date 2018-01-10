a930c90ad3d4d20a10b839d8c0ffcbbe909d6250
TAMIR YARDENI <tamir.yardeni1@gmail.com>
2018-01-10 17:10:35 +0200

    Added presonalAreaActivity

---
+++ app/src/main/java/tmrapps/getinshapeapp/PersonalArea/PersonalAreaActivity.java
@@ -0,0 +1,13 @@
+package tmrapps.getinshapeapp.PersonalArea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_area);
+    }
+}
